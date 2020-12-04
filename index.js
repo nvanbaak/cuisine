bb51d4a17aa338f69802ec1de9fc47722d11fba1
Nikolai <nvanbaak@gmail.com>
2020-12-04 14:26:01 -0800

    added phaser boilerplate

---
+++ index.js
@@ -0,0 +1,31 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 1600,
+    height: 900,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 400 },
+            debug: false
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+}
+
+function preload () {
+
+}
+
+
+function create () {
+
+}
+
+
+function update () {
+
+}
